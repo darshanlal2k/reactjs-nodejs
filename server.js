0dcfb2c33fc7f6d279c6151b0224be7bf343fa72
darshanlal2k <darshanlal2k@gmail.com>
2023-12-06 20:44:28 +0530

    changes in api

--- server.js
+++ server.js
@@ -1,36 +1,50 @@
-console.log("darshan lal");
+// console.log("darshan lal");
 const express = require('express');
+var connection = require('./db');
 const bodyParser = require('body-parser');
 const app = express();
+const PORT = 5000;
 app.use(bodyParser.json());
-
 const cors = require('cors');
 app.use(cors());
 
-app.use(express.json);
-
-app.post('/api', (request, response) => {
-    const formData = req.body;
-    // Process the received data
-    console.log(formData);
-    response.status(200).send('Data received');
-    response.send("from server");
+app.use("/", (req, res) => {
+    console.log(req.body);
+    res.send("Hello i m from server");
 })
 
-const { Client } = require('pg')
-const client = new Client({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'root',
-    database: 'employee',
-    port: 5432,
+app.listen(PORT, () => {
+    console.log("port started at 5000");
 });
-client.connect(function (err) {
-    err ? console.log("Database not connected") : console.log("Database connected");
-});
+// app.get('/', (req, res) => {
+//     console.log(req.body);
+//     connection.query('select * from users', (err, res) => {
+//         if (!err) {
+//             console.log(res.rows);
+//         }
+//         else {
+//             console.log(err);
+//         }
+//     });
+//     res.send('Hello from our server!');
+// })
+
+// app.post('/api', (request, response) => {
+//     const formData = request.body;
+//     // Process the received data
+//     console.log(formData);
+//     response.status(200).send('Data received');
+//     response.send("from server");
+// })
 
 
 
-app.listen(5000, (error) => {
-    error ? console.log("port error") : console.log("port started at 5000");
-});
+// connection.query('select * from users', (err, res) => {
+//     if (!err) {
+//         console.log(res.rows);
+//     }
+//     else {
+//         console.log(err);
+//     }
+// });
+
